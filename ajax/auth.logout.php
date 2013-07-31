c5026c49917351f08baf36dfc8261119c57bb526
Kaleb Clark <kaleb@abraxxus.net>
2013-07-30 19:56:17 -0700

    Mostly working auth system, and database changes

---
+++ ajax/auth.logout.php
@@ -0,0 +1,8 @@
+<?
+include('../inc.php');
+$user->logout();
+?>
+<div class="container content_box">
+  <h3>Come back soon!</h3>
+  <button onClick="emerge.reload_page();">Back to site</button>
+</div>
